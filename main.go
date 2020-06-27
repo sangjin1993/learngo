cd1380e198527d5eea8fab5b464da82efc9306cc
sangjin1993 <hardoner@naver.com>
2020-06-27 17:32:07 +0900

    1.1Packages and Imports

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/sangjin1993/learngo/something"
+)
 
 func main() {
 	fmt.Println("Hello world!")
+	something.SayHello()
+	//something.sayBye() 소문자로 시작하는 함수는 export가 안됌.
 }
---
+++ something/something.go
@@ -0,0 +1,14 @@
+package something
+
+import "fmt"
+
+// 소문자로 시작하는 함수
+func sayBye() {
+	fmt.Println("Bye")
+}
+
+// 대문자로 시작하는 함수
+
+func SayHello() {
+	fmt.Println("Hello")
+}
