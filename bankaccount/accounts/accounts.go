94c8ee6c4d7b8488b24a7903c0ba796b665ee5c9
sangjin1993 <hardoner@naver.com>
2020-07-01 21:00:36 +0900

    2.1 Methods part One

--- bankaccount/accounts/accounts.go
+++ bankaccount/accounts/accounts.go
@@ -14,3 +14,15 @@ func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
 }
+
+// Deposit x amount on your account
+// receiver 함수이름(매개변수)
+// receiver 규칙 struct의 첫 글자를 따서 소문자로 지어야 한다(a Account)
+func (a Account) Deposit(amount int) {
+	a.balance += amount
+}
+
+// Balance of your account
+func (a Account) Balance() int {
+	return a.balance
+}
--- bankaccount/main.go
+++ bankaccount/main.go
@@ -9,4 +9,7 @@ import (
 func main() {
 	account := accounts.NewAccount("nico")
 	fmt.Println(account)
+	account.Deposit(10)
+	fmt.Println(account.Balance())
+
 }
