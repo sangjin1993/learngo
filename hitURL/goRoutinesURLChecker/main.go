972f1b2443817142c1c82eec948f7fb83c53f562
sangjin1993 <hardoner@naver.com>
2020-07-06 16:59:43 +0900

    3.6 URLChecker + Go Routines

---
+++ hitURL/goRoutinesURLChecker/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+type result struct {
+	url    string
+	status string
+}
+
+var errRequestFaild = errors.New("Request failed")
+
+func main() {
+	// 초기화 필요!!
+	// var results map[string]string
+	// var results = map[string]string{}
+	// make를 활용하여 초기화 할수 있음
+	c := make(chan result)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+	// 초기화 되지 않은 map에 추가하면 panic이 발생할수도 있음
+	// results["hello"] = "Hello"
+	// URL: OK | FAILED
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+}
+
+// chan<- 추가하면 send only
+func hitURL(url string, c chan<- result) {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	status := "OK"
+	if err != nil || resp.StatusCode >= 400 {
+		status = "FAILED"
+	}
+	c <- result{url: url, status: status}
+}
